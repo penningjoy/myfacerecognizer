9e1814c1c8a9efad00624b2cedecf2f4703ccd1a
Joydeep Banerjee <joydeepbanerjee2@gmail.com>
2021-04-02 16:49:53 -0400

    Code spruced up; Comments added

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# myfacerecognizer
+# My Face Recognizer
 
-### A simple face recognition program built using Python, Face_Recognition library and OpenCV
+#### A simple face recognition program built using Python, Face_Recognition library and OpenCV
+
+<br>
+
+The program is capable to do face recognition on Unknown pictures by learning faces from a Known set. The face_recognition library
+is used for face encodings, finding the face locations in a picture - the model used for face detection on unknown set is Convolutional
+Neural Network (cnn), and Comparison.
+
+The project has two folders --
+-  known_faces containing the faces used for training and 
+-  unknown_faces containing the faces used for testing / evaluation
+
+## Usage
+```
+pip install -r requirements.txt
+
+python face_recognition_example.py
+
+```
+
--- face_recognition_example.py
+++ face_recognition_example.py
@@ -1,15 +1,25 @@
+'''
+Program - Natural Language Processing
+Developer - Joydeep Banerjee
+Description - The program is capable to do face recognition on Unknown pictures 
+              by learning faces from a Known set. The face_recognition library
+              is used for face encodings, finding the face locations in a picture - 
+              the model used for face detection on unknown set is Convolutional
+              Neural Network (cnn), and Comparison.
+ '''
+
 import face_recognition
 import os
 import cv2
 
-known_faces_dir = "known_faces"
-unknown_faces_dir = "unknown_faces"
+KNOWN_FACES_DIR = "known_faces"
+UNKNOWN_FACES_DIR = "unknown_faces"
 # Higher less false positives
-Tolerence = 0.6
+TOLERANCE = 0.6
 
-frame_thickness = 3
-font_thickness = 2
-model = 'cnn'
+FRAME_THICKNESS = 3
+FONT_THICKNESS = 2
+MODEL = 'cnn'
 known_faces = []
 unknown_faces = []
 known_names = []
@@ -17,10 +27,10 @@ known_names = []
 print(" Fetching known faces")
 
 #Looping through the files in known faces folder
-for subdir in os.listdir(known_faces_dir):
-    for filename in os.listdir(f"{known_faces_dir}/{subdir}"):
-        image = face_recognition.load_image_file(f"{known_faces_dir}/{subdir}/{filename}")
-        joydeep_encoding = face_recognition.face_encodings(image)[0]
+for subdir in os.listdir(KNOWN_FACES_DIR):
+    for filename in os.listdir(f"{KNOWN_FACES_DIR}/{subdir}"):
+        image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{subdir}/{filename}")
+        joydeep_encoding = face_recognition.face_encodings(image)[0]  # Encodes pictures into indexes
         known_faces.append(joydeep_encoding)
         known_names.append(subdir) # Here folder names would become rectangle labels
 
@@ -28,38 +38,39 @@ for subdir in os.listdir(known_faces_dir):
 print(" Fetching unknown faces")
 
 #Looping through the files in known faces folder
-for subdir in os.listdir(unknown_faces_dir):
-    for filename in os.listdir(f"{unknown_faces_dir}/{subdir}"):
-        image = face_recognition.load_image_file(f"{unknown_faces_dir}/{subdir}/{filename}")
-        locations = face_recognition.face_locations(image, model= model)
+for subdir in os.listdir(UNKNOWN_FACES_DIR):
+    for filename in os.listdir(f"{UNKNOWN_FACES_DIR}/{subdir}"):
+        image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{subdir}/{filename}")
+        locations = face_recognition.face_locations(image, model= MODEL) # recognize the faces (cordinates) in the pictures
         encodings = face_recognition.face_encodings(image, locations)
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # Preparing the image to be usable by OpenCV - 
+        # converts an image from one color space to another
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) 
 
 
 
-for face_encoding, face_location in zip(encodings, locations):
-    results = face_recognition.compare_faces(known_faces, face_encoding, Tolerence)
-    match = None
+        for face_encoding, face_location in zip(encodings, locations):
+            # Compares the Known and the Unknown lists and returns a list of booleans( True / False)
+            results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE) 
+            match = None
 
-    if True in results:
-        match = known_names[results.index(True)]
-        print(f' - {match} from {results}')
-        top_left = (face_location[3], face_location[0])
-        bottom_right = (face_location[1], face_location[2])
-        color = (255, 0, 0)
-        cv2.rectangle(image, top_left, bottom_right, color, frame_thickness)
-        top_left = (face_location[3], face_location[2])
-        bottom_right = (face_location[1], face_location[2] + 22)
-        cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
-        cv2.putText(image, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), font_thickness)
+            if True in results:
+                match = known_names[results.index(True)]
+                print(f' - {match} from {results}')
+                # This will put a rectangle on the detected face 
+                top_left = (face_location[3], face_location[0])
+                bottom_right = (face_location[1], face_location[2])
+                color = [255, 0, 0]
+                cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
 
-
-# Show image
-cv2.imshow(filename, image)
-cv2.waitKey(0)
-cv2.destroyWindow(filename)
+                # This is for the label under the top rectangle 
+                top_left = (face_location[3], face_location[2])
+                bottom_right = (face_location[1], face_location[2] + 22)
+                cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+                cv2.putText(image, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS)
 
 
-
-
-
+        # Show image
+        cv2.imshow(filename, image)
+        cv2.waitKey(0)
+        cv2.destroyWindow(filename)
